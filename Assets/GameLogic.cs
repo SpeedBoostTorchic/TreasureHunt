042b307f36f8377febf9672f26f12a6c2f087a78
Charles Yu <cfy220@nyu.edu>
2016-09-20 14:35:43 -0400

    First Commit - Contains most assets, minus the crazy rock stuff

---
+++ Assets/GameLogic.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	bool playerWon = false; //used to end the game
+
+	public Text text; //UI text; used to display messages to the player
+	public Transform player; //the player's location
+	public Transform treasure; //the goal's location
+
+	//landmarks
+	public Transform palm;
+	public Transform bottle;
+	public Transform shadow;
+	public Transform oasis;
+
+	void Start(){
+		treasure = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (playerWon) {
+			//victory messages
+			text.text = "You found the treasure! Congratulations!";
+		
+		//Allows the player to pickup the treasure if they are close enough
+		} else if ((player.position - treasure.position).magnitude < 5f) {
+			text.text = "Here it is! Press [SPACE] to collect the treasure!";
+
+			if (Input.GetKeyUp (KeyCode.Space)) {
+				playerWon = true;
+			}
+		} // End player win condition
+
+		//Hints are listed here
+		if ((player.position - palm.position).magnitude < 5f) {
+			text.text = "Is that? A message in a bottle south east of here?";
+		}
+		if ((player.position - bottle.position).magnitude < 5f) {
+			text.text = "It says to head to the shadow of the great stone, north.";
+		}
+		if ((player.position - shadow.position).magnitude < 10f) {
+			text.text = "Follow the shadow North-West until you reach an oasis";
+		}
+		if ((player.position - oasis.position).magnitude < 15f) {
+			text.text = "Almost there! Just North and to the East to the trasure!";
+		}
+	
+	} //End void Update()
+} 
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public Camera mainCam;
+	public GameObject player;
+	private float movementSpeed = 5;
+
+	// Update is called once per frame
+	void Update () {
+		//movement script
+		if (Input.GetKey (KeyCode.A) && player.transform.position.x > -33) {
+			player.transform.position += new Vector3 (-movementSpeed, 0, 0) * Time.deltaTime; //Movement; time independant
+			player.transform.eulerAngles = new Vector3 (0, 270, 0); //rotate to face movement direction
+		}//end leftward movement
+
+		if (Input.GetKey (KeyCode.D) && player.transform.position.x < 62 ) {
+			player.transform.position += new Vector3 (movementSpeed, 0, 0) * Time.deltaTime; //Movement; time independant
+			player.transform.eulerAngles = new Vector3 (0, 90, 0); //rotate to face movement direction
+		}//end rightward movement
+
+		if (Input.GetKey (KeyCode.W) && player.transform.position.z < 158 ) {
+			player.transform.position += new Vector3 (0, 0, movementSpeed) * Time.deltaTime; //Movement; time independant
+			player.transform.eulerAngles = new Vector3 (0, 0, 0); //rotate to face movement direction
+		}//end forward movement
+
+		if (Input.GetKey (KeyCode.S) && player.transform.position.z > -36) {
+			player.transform.position += new Vector3 (0, 0, -movementSpeed) * Time.deltaTime; //Movement; time independant
+			player.transform.eulerAngles = new Vector3 (0, 180, 0); //rotate to face movement direction
+		}//end backward movement
+
+		//this script moves the camera along with the player
+		Camera.main.transform.position = transform.position + new Vector3(0f,5f,-9f);
+	}
+}
